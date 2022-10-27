eba6420058726ac4f5d3334afa29386ec70147e0
WandersonJunio <wanderson.junioantunes@outlook.com>
2022-10-26 23:40:42 -0300

    fix: changing serverless logical

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,8 @@ import serverless from "serverless-http";
 
 const app = express();
 
-app.get("/", function (req, res) {
-    console.log("console")
-    res.send("Hello worlds");
+app.get("", function (req, res) {
+    return res.send("Hello lambda");
 })
 
-export default serverless(app);
+export const handler = serverless(app);
